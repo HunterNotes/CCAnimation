dadfb8c52c7cf953be0563b957e11eafab66ebe2
cc <1102170937@qq.com>
2016-10-10 17:43:38 +0800

    增加咻一咻动画、摇奖动画效果图GIF

--- CCAnimation/点赞/TouchStarVC.m
+++ CCAnimation/点赞/TouchStarVC.m
@@ -12,7 +12,7 @@
 
 @interface TouchStarVC ()
 {
-    __weak IBOutlet ZView *_levelView;
+    ZView *_levelView;
 }
 
 @end
@@ -21,8 +21,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor redColor];
-    //代码初始化
+
+    _levelView = [[ZView alloc] init];
+    [self.view addSubview:_levelView];
+    [_levelView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.offset(135);
+        make.top.offset(211);
+        make.size.mas_equalTo(CGSizeMake(141, 24));
+    }];
+    
+    //半星，带上浮效果
     ZView *view = [[ZView alloc] init];
     view.iconColor = [UIColor orangeColor];
     view.iconSize = CGSizeMake(20, 20);
@@ -36,12 +44,12 @@
     [self.view addSubview:view];
     
     [view mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(self.view).mas_offset(145);
-        make.top.equalTo(self.view).mas_offset(150);
-        make.size.mas_equalTo(CGSizeMake(_levelView.frame.size.width, _levelView.frame.size.height));
+        make.left.offset(135);
+        make.top.offset(150);
+        make.size.mas_equalTo(CGSizeMake(141, 24));
     }];
     
-    //storyboard生成
+    //整星，不带上浮效果
     _levelView.backgroundColor = [UIColor clearColor];
     _levelView.iconColor = [UIColor redColor];
     _levelView.canScore = YES;
@@ -55,6 +63,7 @@
     [_levelView setScoreBlock:^(float leve) {
         NSLog(@"点赞：%.1f", leve);
     }];
+
 }
 
 @end
